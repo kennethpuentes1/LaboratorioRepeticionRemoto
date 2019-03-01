29e7e5bc8c2e48747f33b61ebc91234e263dfa1d
Kenneth Puentes <47454755+kennethpuentes1@users.noreply.github.com>
2019-03-01 12:43:40 -0500

    Ejercicio dosr esuelto

--- positivos.py
+++ positivos.py
@@ -1,5 +1,17 @@
-x=int(input("Digite un numero entero,para determinar si es par"))
-if x%2==0:
-    print("El numero entero es es par")
+contPar=0
+x = int(input("Cuantos valores va a poner?"))
+
+if x > 100:
+    print("Demasiados valores :P")
 else:
-     print("El numero entero no es par")
+    print("Suficientes valores")
+
+for y in range(1,x,1):
+    z= int(input("Escriba el numero "))
+    
+    if z % 2==0:
+       contPar= contPar+1
+       print("Es par,valores postivos:" ,contPar)
+    elif z%2!=0:
+        print("Es impar")
+        break
