51cd8a203abb49c2f04e6a50d8b8fe6581c32ebc
Kenneth Puentes <47454755+kennethpuentes1@users.noreply.github.com>
2019-03-01 13:25:22 -0500

    Promedio

---
+++ Promedio.py
@@ -0,0 +1,7 @@
+x=int(input("Este programas sirve para calcular el promedio de ciertas notas. Cuantas notas quieres promediar?"))
+notas = 0
+for x in range (x):
+    numeros=int(input("Valor de la nota:"))
+    notas += numeros
+Promedio = notas/x
+print("El promedio  de las notas es:" ,Promedio)
