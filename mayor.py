272938c7737ef7ce7f02c232f0b70ff5baa79993
Puentes Alucema <000399838@bga.upb>
2019-03-01 10:48:46 -0500

    Mi primer commit

---
+++ mayor.py
@@ -0,0 +1,5 @@
+x=int(input("Digite un numero entero para poder determinar si es mayor a cero"))
+if x>0:
+   print("El numero entero es mayor a cero")
+else:
+   print("El numero entero no es mayor a cero")
